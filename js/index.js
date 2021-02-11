5348188d30998aa0485edda9b9600ac671ebd65c
Mikhail Laptev <56048517+laptevlife@users.noreply.github.com>
2021-02-12 00:30:49 +0400

    slider

--- js/index.js
+++ js/index.js
@@ -76,17 +76,7 @@ const closeNav = () => {
 //     },
 // });
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    centeredSlides: false,
-    slidesPerGroupSkip: 1,
-    grabCursor: true,
-    keyboard: { enabled: true },
-    breakpoints: { 769: { slidesPerView: 2, slidesPerGroup: 2 } },
-    scrollbar: { el: '.swiper-scrollbar' },
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    pagination: { el: '.swiper-pagination', clickable: true }
-  });
+
 
 window.addEventListener('scroll', () => {
     const body = document.querySelector("body");
